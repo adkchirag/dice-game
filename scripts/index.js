3cfd06167d6c20396e5bf77df872f48eac47d2b6
Chirag Adhikari <100039962+adkchirag@users.noreply.github.com>
2024-02-18 23:20:34 +0530

    Update index.js
    Removed unnecessary white spaces and added comments to enhance the code readability.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
 document.querySelector("#rollDice").addEventListener("click", function(){
-
     var randomNumber1 = Math.floor((Math.random()* 6) + 1);
     var randomNumber2 = Math.floor((Math.random()* 6) + 1);
-
     var diceImage1 = document.querySelector(".img1");
+    // dynamically changing src attribute on click of button of dice Image 1
     diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
     diceImage1.setAttribute("alt", "Dice" + randomNumber1);
-
     var diceImage2 = document.querySelector(".img2");
+    // dynamically changing src attribute on click of button of dice Image 2
     diceImage2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
     diceImage2.setAttribute("alt", "Dice" + randomNumber2);
-
     if(randomNumber1 > randomNumber2){
         document.querySelector("h1").textContent = "Player 1 Wins!";
     } else if(randomNumber1 < randomNumber2){
@@ -18,5 +16,4 @@ document.querySelector("#rollDice").addEventListener("click", function(){
     } else {
         document.querySelector("h1").textContent = "Draw!!!";
     }
-    
 });
